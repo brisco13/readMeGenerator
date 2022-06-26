4b1ddc8bbeb8badc01e3fe445bf99b7ae1ddd6f0
Brisco Arechederra <brisco.arch@gmail.com>
2022-06-25 23:59:22 -0600

    moved markup creation to util folder

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // required
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Arithmetic = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // array of questions
 const questions = [
@@ -21,35 +23,12 @@ const questions = [
     "Please provide your email address:",
     "Please provide instructions to Contact you:"
 ];
-let readMeText = [];
 
 // readMe guide/template provided by https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
 function writeToFile(fileName, data) {
-    readMeText.push(`# ${data.name} \n\n`);
-    if (data.license.includes("MIT License ")){
-        readMeText.push(`[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)    `)} 
-    if (data.license.includes("CC BY 4.0 License ")){
-        readMeText.push(`[![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)    `)}
-    if (data.license.includes("GNU GPL v3 License ")){
-        readMeText.push(`[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)    `)}
-    if (data.license.includes("Berkeley Software Distribution 3-Clause License ")){
-        readMeText.push(`[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)    `)}
-    if (data.license.includes("Apache License 2.0 ")){
-        readMeText.push(`[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)     `)}
-    if (data.license.includes("Internet Systems Consortium License ")){
-        readMeText.push(`[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)      `)}
-    readMeText.push(`\n\n## Description\n\n${data.desc} \n\n`);
-    readMeText.push(`## Table of Contents\n\n${data.wInstall? `[Installation](#installation)\n\n` : ""}${data.wUsage? `[Usage](#usage)\n\n` : ""}${data.wCont? `[Contribution](#contribution)\n\n` : ""}[License](#license)\n\n[Features](#features)\n\n[Credits](#credits)\n\n${data.wCont? `[Tests](#tests)\n\n` : ""}[Contact Me](#contact-me)\n\n`);
-    readMeText.push(`${data.wInstall ? `## Installation\n\n ${data.install}\n\n` : ""}` );
-    readMeText.push(`${data.wUsage ? `## Usage\n\n ${data.usage}\n\n`: ""}`);
-    readMeText.push(`${data.wCont ? `## Contribution\n\n${data.cont}\n\n`: ""}`);
-    readMeText.push(`## License: \n\nThis project is licensed under the ${data.license}\n\n---\n\n`);
-    readMeText.push(`## Features: \n\n(list Features here - this is a great place for screenshots!)\n\n`);
-    readMeText.push(`## Credits: \n\n(list Credits here)\n\n`);
-    readMeText.push(`${data.wTest ? `## Tests\n\n${data.test}\n\n`: ""}`);
-    readMeText.push(`## Contact Me: \n\n${data.reach}\n ${data.wGit ? `\n[My Github](https://github.com/${data.git})`: ""}${data.wEmail ? `\n[My Email](mailto:${data.email})`: ""}`);
+  const markup = generateMarkdown(fileName,data);
 
-    fs.writeFile(fileName, readMeText.join(''), (err) =>
+    fs.writeFile(fileName, markup, (err) =>
       err ? console.log(err) : console.log(`${fileName} successfully created!`)
     );
 }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,32 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function generateMarkdown(fileName, data) {
+  let readMeText = [];
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  readMeText.push(`# ${data.name} \n\n`);
+  if (data.license.includes("MIT License ")){
+      readMeText.push(`[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)    `)} 
+  if (data.license.includes("CC BY 4.0 License ")){
+      readMeText.push(`[![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)    `)}
+  if (data.license.includes("GNU GPL v3 License ")){
+      readMeText.push(`[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)    `)}
+  if (data.license.includes("Berkeley Software Distribution 3-Clause License ")){
+      readMeText.push(`[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)    `)}
+  if (data.license.includes("Apache License 2.0 ")){
+      readMeText.push(`[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)     `)}
+  if (data.license.includes("Internet Systems Consortium License ")){
+      readMeText.push(`[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)      `)}
+  readMeText.push(`\n\n## Description\n\n${data.desc} \n\n`);
+  readMeText.push(`## Table of Contents\n\n${data.wInstall? `[Installation](#installation)\n\n` : ""}${data.wUsage? `[Usage](#usage)\n\n` : ""}${data.wCont? `[Contribution](#contribution)\n\n` : ""}[License](#license)\n\n[Features](#features)\n\n[Credits](#credits)\n\n${data.wCont? `[Tests](#tests)\n\n` : ""}[Contact Me](#contact-me)\n\n`);
+  readMeText.push(`${data.wInstall ? `## Installation\n\n ${data.install}\n\n` : ""}` );
+  readMeText.push(`${data.wUsage ? `## Usage\n\n ${data.usage}\n\n`: ""}`);
+  readMeText.push(`${data.wCont ? `## Contribution\n\n${data.cont}\n\n`: ""}`);
+  readMeText.push(`## License: \n\nThis project is licensed under the ${data.license}\n\n---\n\n`);
+  readMeText.push(`## Features: \n\n(list Features here - this is a great place for screenshots!)\n\n`);
+  readMeText.push(`## Credits: \n\n(list Credits here)\n\n`);
+  readMeText.push(`${data.wTest ? `## Tests\n\n${data.test}\n\n`: ""}`);
+  readMeText.push(`## Contact Me: \n\n${data.reach}\n ${data.wGit ? `\n[My Github](https://github.com/${data.git})`: ""}${data.wEmail ? `\n[My Email](mailto:${data.email})`: ""}`);
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  const markup = readMeText.join('');
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
+  return markup;}
 
 module.exports = generateMarkdown;
