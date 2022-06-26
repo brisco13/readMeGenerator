360904f0b95ff7db891bae25e9387b62da02b817
Brisco Arechederra <brisco.arch@gmail.com>
2022-06-25 23:20:48 -0600

    all code complete. readme to be created.

--- index.js
+++ index.js
@@ -1,72 +1,61 @@
-// 
+// required
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
+// array of questions
 const questions = [
-    "What is the title of your project?",                                //0
-    "Please provide a short description of your project:",               //1
-    "Which license is this project covered under?",                      //2
-    "Would you like to provide installation instructions?",        //3
-    "Please provide installation instructions:",                         //4
-    "Would you like to provide usage information? ",                //5
-    "Please provide usage information:",                                 //6
-    "Would you like to provide contribution instructions?",          //7
-    "Please provide contribution guidelines:",                           //8
-    "Would you like to provide test instructions?",                //9
-    "Please provide test instructions:",                                 //10   
-    "Would you like to provide your github information?",          //11
-    "Please provide your github username:",                              //12
-    "Would you like to provide your email address?",               //13
-    "Please provide your email address:",                                //14
-    "Please provide instructions to Contact you:",                         //16
+    "What is the title of your project?",
+    "Please provide a short description of your project:",
+    "Which license is this project covered under?",
+    "Would you like to provide installation instructions?",
+    "Please provide installation instructions:",
+    "Would you like to provide usage information? ",
+    "Please provide usage information:",
+    "Would you like to provide contribution instructions?",
+    "Please provide contribution guidelines:",
+    "Would you like to provide test instructions?",
+    "Please provide test instructions:",  
+    "Would you like to provide your github information?",
+    "Please provide your github username:",
+    "Would you like to provide your email address?",
+    "Please provide your email address:",
+    "Please provide instructions to Contact you:"
 ];
-let readMeText = ["","","","","","","","","","","","","","","",""];
+let readMeText = [];
 
 // readMe guide/template provided by https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
 function writeToFile(fileName, data) {
-    readMeText[0] = `# ${data.name} \n\n`;
-    readMeText[1] = `## Description\n\n${data.desc} \n\n`;
-    readMeText[2] = `## Table of Contents\n\n
-    ${data.wInstall? `[Installation](#install)\n` : ""} 
-    ${data.wUsage? `[Usage](#us)\n` : ""} 
-    ${data.wCont? `[Contribution](#cont)\n` : ""} 
-    [License](#lic)\n
-    [Features](#feat)\n
-    [Credits](#cred)\n
-    ${data.wCont? `[Tests](#test)\n` : ""} 
-    [Contact Me](#contact)\n
-    `;
-    
-    readMeText[3] = `${data.wInstall ? `<a name="install"></a>## Installation\n\n ${data.install}\n\n` : ""}` 
-    
-    readMeText[4] = `${data.wUsage ? `<a name="us"></a>## Usage\n\n ${data.usage}\n\n`: ""}`
-    
-    readMeText[6] = `${data.wCont ? `<a name="cont"></a>## Contribution\n\n${data.cont}\n\n`: ""}`
-
-    readMeText[7] = `<a name="lic"></a>## License: \n\nThis project is licensed under the ${data.license}\n\n---\n\n`
-
-    readMeText[8] = `<a name="feat"></a>## Features: \n\n(list Features here - this is a great place for screenshots!)\n\n`
-
-    readMeText[9] = `<a name="cred"></a>## Credits: \n\n(list Credits here)\n\n`
-
-    readMeText[10] = `${data.wTest ? `<a name="test"></a>##Tests\n\n${data.test}\n\n`: ""}`
-
-    readMeText[11] = `<a name="contact"></a>## Contact Me: ${data.wGit ? `\n\n[My Github](https://github.com/${data.git})`: ""}${data.wEmail ? `\n\n[My Email](mailto:${data.email})`: ""}\n\n${data.reach}\n\n`
-
+    readMeText.push(`# ${data.name} \n\n`);
+    if (data.license.includes("MIT License")){
+        readMeText.push(`[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)    `)} 
+    if (data.license.includes("CC BY 4.0 License")){
+        readMeText.push(`[![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)    `)}
+    if (data.license.includes("GNU GPL v3 License")){
+        readMeText.push(`[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)    `)}
+    if (data.license.includes("Berkeley Software Distribution 3-Clause License")){
+        readMeText.push(`[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)    `)}
+    if (data.license.includes("Apache License 2.0")){
+        readMeText.push(`[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)     `)}
+    if (data.license.includes("Internet Systems Consortium License")){
+        readMeText.push(`[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)      `)}
+    readMeText.push(`\n\n## Description\n\n${data.desc} \n\n`);
+    readMeText.push(`## Table of Contents\n\n${data.wInstall? `[Installation](#installation)\n\n` : ""}${data.wUsage? `[Usage](#usage)\n\n` : ""}${data.wCont? `[Contribution](#contribution)\n\n` : ""}[License](#license)\n\n[Features](#features)\n\n[Credits](#credits)\n\n${data.wCont? `[Tests](#tests)\n\n` : ""}[Contact Me](#contact-me)\n\n`);
+    readMeText.push(`${data.wInstall ? `## Installation\n\n ${data.install}\n\n` : ""}` );
+    readMeText.push(`${data.wUsage ? `## Usage\n\n ${data.usage}\n\n`: ""}`);
+    readMeText.push(`${data.wCont ? `## Contribution\n\n${data.cont}\n\n`: ""}`);
+    readMeText.push(`## License: \n\nThis project is licensed under the ${data.license}\n\n---\n\n`);
+    readMeText.push(`## Features: \n\n(list Features here - this is a great place for screenshots!)\n\n`);
+    readMeText.push(`## Credits: \n\n(list Credits here)\n\n`);
+    readMeText.push(`${data.wTest ? `##Tests\n\n${data.test}\n\n`: ""}`);
+    readMeText.push(`## Contact Me: \n\n${data.reach}\n ${data.wGit ? `\n[My Github](https://github.com/${data.git})`: ""}${data.wEmail ? `\n[My Email](mailto:${data.email})`: ""}`);
 
     fs.writeFile(fileName, readMeText.join(''), (err) =>
       err ? console.log(err) : console.log('Success!')
     );
 }
 
-// TODO: Create a function to initialize app
-function init() {
-    ask();
-}
-
 // inquirer prompts
-function ask() {
+function init() {
     inquirer
         .prompt([
             //Project Name
@@ -83,12 +72,10 @@ function ask() {
             },
             //Project license
             {
-                type: 'list',
+                type: 'checkbox',
                 name: 'license',
                 message: questions[2],
-                choices: [  "MIT License", 
-                            "CC BY 4.0 License", "GNU GPL v3 License", "Berkeley Software Distribution License", 
-                            "Apache License 2.0", "Internet Systems Consortium License"]
+                choices: [  "MIT License", "CC BY 4.0 License", "GNU GPL v3 License", "Berkeley Software Distribution License", "Apache License 2.0", "Internet Systems Consortium License"]
             },
             //install instructions
             {
