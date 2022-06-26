d53feff30b6750064e0cf75c42ba7cd7c2886926
Brisco Arechederra <brisco.arch@gmail.com>
2022-06-25 23:25:49 -0600

    readme.md complete - can confirm this app works well.

--- index.js
+++ index.js
@@ -26,17 +26,17 @@ let readMeText = [];
 // readMe guide/template provided by https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
 function writeToFile(fileName, data) {
     readMeText.push(`# ${data.name} \n\n`);
-    if (data.license.includes("MIT License")){
+    if (data.license.includes("MIT License ")){
         readMeText.push(`[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)    `)} 
-    if (data.license.includes("CC BY 4.0 License")){
+    if (data.license.includes("CC BY 4.0 License ")){
         readMeText.push(`[![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)    `)}
-    if (data.license.includes("GNU GPL v3 License")){
+    if (data.license.includes("GNU GPL v3 License ")){
         readMeText.push(`[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)    `)}
-    if (data.license.includes("Berkeley Software Distribution 3-Clause License")){
+    if (data.license.includes("Berkeley Software Distribution 3-Clause License ")){
         readMeText.push(`[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)    `)}
-    if (data.license.includes("Apache License 2.0")){
+    if (data.license.includes("Apache License 2.0 ")){
         readMeText.push(`[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)     `)}
-    if (data.license.includes("Internet Systems Consortium License")){
+    if (data.license.includes("Internet Systems Consortium License ")){
         readMeText.push(`[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)      `)}
     readMeText.push(`\n\n## Description\n\n${data.desc} \n\n`);
     readMeText.push(`## Table of Contents\n\n${data.wInstall? `[Installation](#installation)\n\n` : ""}${data.wUsage? `[Usage](#usage)\n\n` : ""}${data.wCont? `[Contribution](#contribution)\n\n` : ""}[License](#license)\n\n[Features](#features)\n\n[Credits](#credits)\n\n${data.wCont? `[Tests](#tests)\n\n` : ""}[Contact Me](#contact-me)\n\n`);
@@ -46,11 +46,11 @@ function writeToFile(fileName, data) {
     readMeText.push(`## License: \n\nThis project is licensed under the ${data.license}\n\n---\n\n`);
     readMeText.push(`## Features: \n\n(list Features here - this is a great place for screenshots!)\n\n`);
     readMeText.push(`## Credits: \n\n(list Credits here)\n\n`);
-    readMeText.push(`${data.wTest ? `##Tests\n\n${data.test}\n\n`: ""}`);
+    readMeText.push(`${data.wTest ? `## Tests\n\n${data.test}\n\n`: ""}`);
     readMeText.push(`## Contact Me: \n\n${data.reach}\n ${data.wGit ? `\n[My Github](https://github.com/${data.git})`: ""}${data.wEmail ? `\n[My Email](mailto:${data.email})`: ""}`);
 
     fs.writeFile(fileName, readMeText.join(''), (err) =>
-      err ? console.log(err) : console.log('Success!')
+      err ? console.log(err) : console.log(`${fileName} successfully created!`)
     );
 }
 
@@ -75,7 +75,7 @@ function init() {
                 type: 'checkbox',
                 name: 'license',
                 message: questions[2],
-                choices: [  "MIT License", "CC BY 4.0 License", "GNU GPL v3 License", "Berkeley Software Distribution License", "Apache License 2.0", "Internet Systems Consortium License"]
+                choices: [  "MIT License ", "CC BY 4.0 License ", "GNU GPL v3 License ", "Berkeley Software Distribution License ", "Apache License 2.0 ", "Internet Systems Consortium License "]
             },
             //install instructions
             {
@@ -164,7 +164,6 @@ function init() {
             }, 
         ])
     .then((data) => {
-        console.log(data)
         const fileName = `${data.name.toLowerCase().split(' ').join('')}.md`;
         writeToFile(fileName,data)
     })
---
+++ readme.md
@@ -0,0 +1,64 @@
+# Read Me Generator    
+
+## Description
+
+This app allows you to input answers to prompts in the command line via node.js to generate a read me file. 
+
+## Table of Contents
+
+[Installation](#installation)
+
+[Usage](#usage)
+
+[Contribution](#contribution)
+
+[License](#license)
+
+[Features](#features)
+
+[Credits](#credits)
+
+[Tests](#tests)
+
+[Contact Me](#contact-me)
+
+## Installation
+
+ Prior to use, please run a "npm install" to ensure the latest versions of node.js are fetched.
+
+## Usage
+
+ This app may be used by whomever in any capacity.
+
+## Contribution
+
+This project will not be accepting contributions, but thank you for your interest!
+
+## License: 
+
+This project is licensed under the MIT License,Berkeley Software Distribution License,Internet Systems Consortium License
+
+---
+
+## Features: 
+
+- Simply follow the prompts to create a readme.md file:
+![picture1](./assets/images/picture1.png "Picture1")
+- This read me was made with this!
+- Creates a completely formatted readme.md file with prompts to improve
+- Multiple license badges may be included and added to the top of your project
+
+
+## Credits: 
+
+- Brisco Arechederra (independent Project)
+
+## Tests
+
+This is not a sophisticated app - there are no additional tests provided.
+
+## Contact Me: 
+
+Please feel free to reach out to me on github with any questions!
+ 
+[My Github](https://github.com/brisco13)
